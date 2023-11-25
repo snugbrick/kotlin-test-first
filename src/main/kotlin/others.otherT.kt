6be18b79b255f843516df766641425f7261c4be9
圣迹 <76844630+snugbrick@users.noreply.github.com>
2023-11-26 03:11:41 +0800

    Initial commit

---
+++ src/main/kotlin/others.otherT.kt
@@ -0,0 +1,7 @@
+/**
+@version 1.0.0 2023.11.26 03:06
+@author MiracleUR -> github.com/snugbrick
+ */
+fun intTest(num: Int){
+    println("Hello $num")
+}
---
+++ src/main/kotlin/test2.kt
@@ -0,0 +1,7 @@
+/**
+@version 1.0.0 2023.11.26 02:58
+@author MiracleUR -> github.com/snugbrick
+ */
+fun stringTest(name: String){
+    println("Hello $name")
+}
